8742bd20e061bbda7884839741aae5d4a4f15b02
maggiewiseman <maggie.wiseman@gmail.com>
2016-02-28 11:48:47 -0500

    first upload of javascript files

---
+++ rockPaperScissors.js
@@ -0,0 +1,12 @@
+//rock paper scissors game
+
+var check = function(first, second){
+	if (first ==== second) {
+		return "tie";
+	} else if (first === "rock" & second === "scissors")
+		return "first wins!";
+	} else if (first === )	
+};
+var user1 = prompt("rock, paper or scissors?")
+var user2 = prompt("rock, paper or scissors?")
+check(user1, user2)
