02d86177f6172fa1a5332d3449ff14e68345596f
Maggie Wiseman <maggie.wiseman@gmail.com>
2016-11-11 20:59:15 -0500

    finshed exercise 1 making array given  a range and a step

--- eloquentJavascript/chapter4-no1.js
+++ eloquentJavascript/chapter4-no1.js
@@ -1,30 +1,40 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */ /*global define */
 
 var range = function (start, end, step) {
-  
-  var array1 = [];
-  var i = 0;
-   while(start <= end){
-     array1[i] = start; 
-     start++;
-     i++;
-   }
-  return array1;
-};
-
-console.log(range(2,5));
-
-var sum = function(arrayp) { 
-  console.log("array is:" + arrayp);
-    var total = 0;
-    for(var i = 0; i < arrayp.length; i++) {
-       total = total + arrayp[i];
+    'use strict';
+    var array1 = [],
+      i = 0;
+    if (start < end) {
+      while (start <= end) {
+        array1[i] = start;
+        start = start + step;
+        i++;
+      }
+    } else if (start > end) {
+      while (start >= end) {
+        array1[i] = start;
+        start = start + step;
+        i++;
+      }
     }
+    console.log("array:" + array1);
+    return array1;
+  };
+
+console.log(range(2, 5));
+
+var sum = function (arrayp) {
+  'use strict';
+  var total = 0;
+  for (var i = 0; i < arrayp.length; i++) {
+    total = total + arrayp[i];
+  }
   return total;
 
-}
+};
 
 
-var total2 = sum(range(1, 8));
+var total2 = sum(range(5, 2, -1));
 
 console.log(total2);
 
